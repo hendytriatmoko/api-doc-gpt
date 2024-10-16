19d9f343a91f3a49d1ba408179648e693bf98a1b
Hendi Triatmoko <hendytriatmoko0@gmail.com>
2024-10-16 11:25:59 +0700

    up

--- database/config.js
+++ database/config.js
@@ -1,20 +1,25 @@
 const mysql = require('mysql2');
 
-// Konfigurasi koneksi ke database MySQL
-const db = mysql.createConnection({
+// Konfigurasi pool koneksi ke database MySQL
+const pool = mysql.createPool({
     host: '167.172.95.72', // Alamat host MySQL, bisa juga IP server
-    user: 'binus',      // Nama pengguna MySQL
+    user: 'binus',         // Nama pengguna MySQL
     password: '35536298',  // Password MySQL
-    database: 'apigpt'  // Nama database yang akan diakses
+    database: 'apigpt',    // Nama database yang akan diakses
+    waitForConnections: true, // Menunggu koneksi jika pool penuh
+    connectionLimit: 10,      // Jumlah maksimum koneksi dalam pool
+    queueLimit: 0             // Tidak ada batasan antrian koneksi
 });
 
-// Buka koneksi
-db.connect((err) => {
+// Test koneksi ke database MySQL
+pool.getConnection((err, connection) => {
     if (err) {
         console.error('Error connecting to MySQL:', err.stack);
         return;
     }
-    console.log('Connected to MySQL as id', db.threadId);
+    console.log('Connected to MySQL as id', connection.threadId);
+    connection.release(); // Lepaskan koneksi kembali ke pool
 });
 
-module.exports = db;
+// Ekspor pool koneksi agar bisa digunakan di file lain
+module.exports = pool;
